85bf59df0fa479a4d4a370ef9488d298d8be298d
Shivaot <shivatiwari757@gmail.com>
2019-09-29 23:46:39 +0530

    Emmit newMessage on createMeassage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to Server');
-
-    socket.emit('createMessage', {
-        to: 'jen@example.com',
-        text: 'Hey, this is Shiva.'
-    });
 });
 
 socket.on('disconnect', function() {
